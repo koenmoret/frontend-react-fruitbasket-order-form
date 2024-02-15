ba22fba0debd532de25fc9e4d4de6cc44806b478
koenmoret <koen@moret.info>
2024-02-15 17:20:00 +0100

    update

--- src/components/fruitbox.jsx
+++ src/components/fruitbox.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line react/prop-types
-function Fruitbox({ image,setName,count, setCount }) {
+function Fruitbox({image, setName, count, setCount}) {
 
     const setCounter = (action) => {
         setCount((val) => val + action)
@@ -9,12 +9,18 @@ function Fruitbox({ image,setName,count, setCount }) {
         <div className="fruit-box">
             <img src={image} alt={`Een plaatje met ${setName}`}/>
             <h1>{setName}</h1>
-            <button className="setCount" disabled={count === 0} onClick={() => { count > 0 &&
-                setCounter(-1)
-            }}>-
-            </button>
-            <p>{count}</p>
-            <button className="setCount" onClick={() => { setCounter(1) }}> + </button>
+            <div>
+                <button className="setCount" disabled={count === 0} onClick={() => {
+                    count > 0 &&
+                    setCounter(-1)
+                }}>-
+                </button>
+                <p>{count}</p>
+                <button className="setCount" onClick={() => {
+                    setCounter(1)
+                }}> +
+                </button>
+            </div>
         </div>
     );
 }
